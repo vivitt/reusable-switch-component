7e0094c963fd94584c1119ec7d97fe86e8cd2325
vivitt <viviviyanez@gmail.com>
2023-10-20 13:32:54 +0200

    add swicher styles

--- switcher.js
+++ switcher.js
@@ -1,8 +1,7 @@
 export default class VivittSwitcher extends HTMLElement{
     constructor() {
     super();
-        this.name = 'switcher'
-        this.state = 'off';
+        this.name = 'switcher' 
     }   
     static get observedAttributes() {
         return ['checked']
@@ -13,69 +12,74 @@ export default class VivittSwitcher extends HTMLElement{
     set checked(value) {
         this.toggleAttribute('checked', value)
     }
-    attributeChangedCallback(name, oldValue, newValue) {
+ 
+
+    connectedCallback() {
+        const shadow = this.attachShadow({ mode: 'open', delegatesFocus: true }); 
+        
+        this.wrapper = document.createElement('span')
+        this.wrapper.setAttribute('class','vivitt-switcher');
+    
+        this.__style = document.createElement("style")
+     
+        this.shadowRoot.appendChild(this.__style);
+
+        this.__style.innerHTML = `   
+        input {
+            opacity: 0;
+            width: 0;
+            height: 0;
+        } 
+        label {
+            display: block;
+            --color: black;
+            --container-width: 100px;
+            width: var(--container-width);
+            height: calc(var(--container-width) / 2);
+            border: 1px solid var(--color);
+            border-radius: 30px;
+            position: relative;
+            cursor: pointer;
+            color: var(--color)
+           
+        }
+        
+        input:focus ~ label  {
+            outline: solid 2px   
+        }
+
+        label::after {
+            content: "🌚";
+            position: absolute;
+            //background-color: var(--color);
+            --size: calc(var(--container-width) / 2 );
+            font-size: var(--size);
+            width: var(--size);
+            height: var(--size);
+            top: -6px;
+            left: 0px;
+            transition: left 300ms;
+        }
+        input:checked ~ label::after {
+            content: "🌞";
+            left: var(--size)    
+        }
+        `
+        this.shadowRoot.appendChild(this.wrapper)
+        this.wrapper.innerHTML = `
+                <input type="checkbox" id="switcher" />
+                <label for=switcher>                   
+            `;
+        this.addEventListener('click', () => {
+            this.checked = !this.checked
+        })
+    }
+    attributeChangedCallback(name) {
         if (name === 'checked') {
             this.setAttribute('aria-checked', this.checked.toString())
         } 
-    }
-    connectedCallback() {
-        const shadow = this.attachShadow({ mode: 'open', delegatesFocus: true }); 
-        this.mainComp = document.createElement('span')
-        this.mainComp.setAttribute('class','vivitt-switcher');
-
-        this.customStyle = "";
-        this.__style = document.createElement("style")
-        this.__style.textContent = ""
-
-        this.shadowRoot.appendChild(this.__style);
-        
-        this.mainComp.innerHTML = `
-            <style>
-                .vivitt-switcher input {
-                  
-                   
-                }
-                .vivitt-switcher label {
-                    display: block;
-                    --color: black;
-                    --container-width: 100px;
-                    width: var(--container-width);
-                    height: calc(var(--container-width) / 2);
-                    border: 1px solid var(--color);
-                    position: relative;
-                    cursor: pointer;
-                }
-                .vivitt-switcher label::after {
-                    content: "";
-                    position: absolute;
-                    background-color: var(--color);
-                    --size: calc(var(--container-width) / 2 );
-                    width: var(--size);
-                    height: var(--size);
-                    top: 0px;
-                    left: 0px;
-                    transition: left 300ms;
-                }
-                .vivitt-switcher input:checked ~ label::after{
-                    left: calc(var(--container-width) / 2 );
-                    transition: left 300ms;
-                
-                }
-            </style>
-            <input
-            type="checkbox"
-            id="switcher"
-            role="switch"
-            />
-            <label for="switcher"></label>                                      
-            `;
-        this.shadowRoot.append(this.mainComp)
-        this.setAttribute('aria-checked', this.checked.toString())
- 
-        this.addEventListener('click', () => {
-        this.checked = !this.checked
-        })
-    }
+    }  
+   
 }
 
 customElements.define("vivitt-switcher", VivittSwitcher)
